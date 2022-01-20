266dece91d73df4b7a9fd89ac9d88c4ee524173e
Ron <rc72095@icloud.com>
2022-01-20 15:02:16 -0500

    Optional chaining operator - ?

---
+++ es2020/es2020.js
@@ -0,0 +1,43 @@
+// Exercise 1: Clean up this code using optional chaining
+let will_pokemon = {
+    pikachu: {
+        species: 'Mouse Pokemon',
+        height: 0.4,
+        weight: 6,
+        power: 'lightning',
+        friend: { 
+            charizard: {
+                species: 'Dragon Pokemon',
+                height: 1.7,
+                weight: 90.5,
+                power: 'fire'
+            }
+        }
+    }
+}
+
+let andrei_pokemon = {
+    raichu: {
+        species: 'Mouse Pokemon',
+        height: 0.8,
+        weight: 30,
+        power: ''
+    }
+}
+
+if (andrei_pokemon && andrei_pokemon.raichu && will_pokemon 
+    && will_pokemon.pikachu && will_pokemon.pikachu.friend 
+    && will_pokemon.pikachu.friend.charizard) {
+        console.log('fight!')
+    } else {
+        console.log('walk away...')
+    }
+
+
+  // my answer:
+
+    if (andrei_pokemon?.raichu && will_pokemon?.pikachu?.friend?.charizard) {
+        console.log("fight");
+    } else {
+        console.log("truce...");
+    }
